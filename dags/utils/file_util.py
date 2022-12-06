86271e6c0684a6440afe1f8221b503694444338a
Jessica Robles <69654419+jessiRob@users.noreply.github.com>
2022-12-05 20:11:15 -0500

    ETL mujeres p1

---
+++ dags/utils/file_util.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def cargar_datos(name):
+    df = pd.read_csv("/opt/airflow/data/" + name + ".csv", sep=',', encoding = 'latin1', index_col=False)
+    return df
+
+def guardar_datos(df, nombre):
+    df.to_csv("/opt/airflow/data/" + nombre + ".csv" , encoding = 'latin1', sep=',', index=False)  
+
+    
+def procesar_datos():
+    pass
+
+
